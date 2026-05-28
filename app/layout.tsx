cfc191b115eecfa6b3b9ad20cf5aa841f901b2e4
kaiwenyao <kaiwen.yao.cn@outlook.com>
2026-05-29 00:20:46 +0100

    add clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,44 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { inter } from "@/app/ui/fonts"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { inter } from "@/app/ui/fonts";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import Navbar from "@/components/Navbar";
-
+import {
+  ClerkProvider,
+  Show,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
+
     <html lang="en">
-      <Navbar />
-      <body className = {`${inter.className} antialiased`}>{children}</body>
-      <SpeedInsights />
+
+      <body className={`${inter.className} antialiased`}>
+        <ClerkProvider>
+        <Navbar />
+          <header className="flex justify-end items-center p-4 gap-4 h-16">
+            <Show when="signed-out">
+              <SignInButton />
+              <SignUpButton>
+                <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                  Sign Up
+                </button>
+              </SignUpButton>
+            </Show>
+            <Show when="signed-in">
+              <UserButton />
+            </Show>
+          </header>
+          {children}
+        </ClerkProvider>
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
