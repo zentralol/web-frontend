4956ed0c99d501ea305aac956dd72b94cef5db36
kaiwenyao <kaiwen.yao.cn@outlook.com>
2026-05-29 00:27:48 +0100

    add custom sign up and sign in page

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,8 +20,9 @@ export default function RootLayout({
     <html lang="en">
 
       <body className={`${inter.className} antialiased`}>
-        <ClerkProvider>
         <Navbar />
+        
+        <ClerkProvider>
           <header className="flex justify-end items-center p-4 gap-4 h-16">
             <Show when="signed-out">
               <SignInButton />
@@ -37,6 +38,7 @@ export default function RootLayout({
           </header>
           {children}
         </ClerkProvider>
+
         <SpeedInsights />
       </body>
     </html>
